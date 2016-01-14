439dc44f642744f190f781bbdea8be5d276c99dc
Jim Veneskey <github@strifecrafter.com>
2016-01-14 07:15:05 -0500

    More experimenting with the Lua stack from C

--- C_modules/demo.c
+++ C_modules/demo.c
@@ -45,11 +45,54 @@ int f(lua_State *L) {
   lua_insert(L, -3);
     // stack = [<args>, 12.3, nil, "Pushed onto the stack", true]
 
+    // remove the topmost value from the stack
+  lua_pop(L, 1);
+    // stack = [<args>, 12.3, nil, "Pushed onto the stack"]
+
+    // remove from an abitrary position (3rd from right - or '12.3' is removed)
+  lua_remove(L, -3);
+    // stack = [<args>, nil, "Pushed onto the stack"]
+
+    // opposite of remove is push... (counting from the left or 'nil')
+  lua_pushvalue(L, 1);
+    // stack = [<args>, nil, "Pushed onto the stack", nil]
+
+    // replace the top value on the stack with entry #3
+//  lua_replace(L, -3);
+    // stack = [<args>, nil, "Pushed onto the stack"]
+
+    // stack = [<args>, nil, "Pushed onto the stack", nil]
+//  lua_replace(L, 2);
+    // stack = [<args>, nil, nil]
+
+    // stack = [<args>, nil, "Pushed onto the stack", nil]
+  lua_replace(L, 3);
+    // stack = [<args>, nil, "Pushed onto the stack"]
+
+    //> f(1, 2, 3, 4)
+    // 6 values are on the stack right now.
+    // stack: 1 2 1 4 nil 'Pushed onto the stack'
+
+  // in the above example, 1, 2, 3, 4 - '3' was replaced with the top of the stack or '1'
+
+  // check and see if the first argument is a string...?
+  int isString = lua_isstring(L, 1);
+  printf("is the first stack item a string? %d\n", isString);
+
+
   int n = lua_gettop(L);
   printf("%d values are on the stack right now.\n", n);
 
 
   print_stack(L);
+
+  if (isString == 1) {
+    printf("The first item is a String\n");
+  } else {
+    printf("The first item is not a String\n");
+  }
+
+
   return 3;
 }
 
